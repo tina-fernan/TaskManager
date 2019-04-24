ace390e2ca26f673c256bdd914a1ea4185a4a49e
tina-fernan <tinaaroom@gmail.com>
2019-04-24 17:09:50 +0200

    TaskManager

--- TaskManager.js
+++ TaskManager.js
@@ -1,22 +1,45 @@
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+   });
+   
 class TaskManager{
-    constructor(readliner){
-        this.readliner=readliner;
+    constructor(){
+        this.task=[];
         
     }
-     ask=function (question) {
-        return new Promise(resolve =>
-        this.readliner.question(question, answer => resolve(answer))
-        );
-      };
-      
+   
+     ask() {
+        return new Promise(resolve =>{
+            const recursiveFn=()=>{
+                this.rl(function(item){
+                    if(item!=="quit"){
+                        this.task.push(item);
+                        recursiveFn();
+                    }else{
+                        resolve();
+                    }
+                });
+            }
+        recursiveFn();
+      });
+    }
+    getTask(){
+        return this.task;
+    }
+    //It is updates 
       
    async talkToPerson(){
-       const first=await this.readliner.ask("what is your task manager?");;
-       const second = await this.readliner.ask("please enter your code");
+       const first=await rl.ask("what is your task manager?");;
+       const second = await rl.ask("please enter your task");
 
-       console.log(`This is  your ${first}${second}`);
-    }
-    
+       console.log(`This is  your ${first}${second} answer.`);
+    };
+
     
 }
-ask();
+const taskManager = new TaskManager();
+taskManager.talkToPerson();
